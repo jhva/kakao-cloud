72ece3152261ef98db0cf2d59522c42526d719b6
DevKJHe <junghun8159@naver.com>
2022-11-09 16:46:27 +0900

    cloud service extenstion and export,import add

---
+++ 11-09/export.js
@@ -0,0 +1,17 @@
+
+
+ const data = Math.PI * Math.SQRT2;
+
+
+ function dataFUnction(){
+    console.log("김정훈펑션")
+}
+
+var grapht={
+    options:{color:"white",thiasdf:"23"},
+    draw:function(){console.log("그림그리기")}
+}
+
+
+export {data,dataFUnction,grapht};
+
