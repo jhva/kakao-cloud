fa62efe8198920b5f5f068d4c82967633fcf22f7
jhva <junghun8158@naver.com>
2022-11-24 17:51:25 +0900

    connection query 페이지번호 매기기

--- DB/11-24/README.md
+++ DB/11-24/README.md
@@ -145,3 +145,209 @@ npm install express morgan multer mysql cookie-parser express-session express-my
 npm install --save-dev nodemon
 ```
 - nodemon : 소스코드를 수정하고 다시 서버를 실행하지않고  재시작을 바로할수있도록 해주는 모듈
+
+### 서버에 데이터 업데이트 시간
+> update.txt에 로그기록
+
+### 프로젝트에 .env 파일을 만들고 필요한 속성을 정의 
+
+```javascript 
+const dotenv = require('dotenv');
+
+dotenv.config();
+```
+
+### post 방식의 파라미터 읽을 수 있도록 설정
+
+```javascript
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+```
+
+### 압축해서 전송하는 옵션설정
+```javascript
+app.use(compression());
+app.use(morgan('combined', { stream: accessLogStream }));
+```
+    
+### 서버구동
+```javascript
+app.listen(app.get('port'), () => {
+    console.log(app.get('port'), '번 포트킴');
+});
+
+//서버에러시
+app.use((err, req, res, next) => {
+//서버에러시
+    console.log(err);
+    req.status(500).send(err.message);
+});
+```
+### 세션설정
+
+```javascript
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: new MariaDBStore(options)
+}))
+```
+### css경로
+```html
+ <link rel="stylesheet" href="/css/common.css"/>
+```
+
+### 로그를 매일 기록하기 위한 설정
+
+```javascript
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+//로그 파일 옵션을 설정
+let accessLogStream = FileStreamRotator.getStream({
+    date_format: 'YYYYMMDD',
+    filename: path.join(logDirectory, 'access-%DATE%.log'),
+    frequency: 'daily',
+    verbose: false
+})
+```
+
+### 정적파일
+- 웹에서는 소스 코드를 제외하고 서버의 데이터를 출력하는 용도가 아닌 파일
+    - HTML
+    - CSS
+    - JS
+    - 이미지
+    - 사운드
+    - 동영상
+
+
+### index.html출력
+```javascript
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html')) //html출력
+    //서버의 데이터 출력못함 - ajax나 fetch api 를 이용해야함
+    // 템플릿 엔진 :res.render(파일경로,데이터)
+    // 템플릿 엔진에 넘겨주는 데이터는 프로그래밍 언어의 데이터
+    
+
+    //json 출력 res.json(데이터)
+    //json문자열의 형태로 데이터를 제공
+
+      //전체 출력할때 꼭 sort를 해줘야한다 sql은 기본적으로 오름차순이기때문에
+    //2개 이상의 데이터를 조회할때는 정렬은 필수
+    // 매개변수의 ? 가없으면 [] 는없애도된다
+});
+```
+
+
+### 데이터 전체 가져오기
+- url
+    - data/all
+```javascript
+app.get('/data/all', (req, res) => {
+
+    connection.query("select * from goods order by itemid desc", [], (err, results, fields) => {
+        if (err) {
+            //err 가 발생한경우
+            // 에러가 발생했다고 데이터를 전송하지 않으면 안됨.
+            res.json({ 'result': false })
+            console.log(err);
+        } else {
+            res.json({ 'result': true, 'list': results });
+            //정상 응답을 한 경우
+        }
+    })
+});
+```
+
+### html 본문 바디에 추가 
+```javascript
+ let btn = document.getElementById('allData_btn');
+
+        btn.addEventListener("click", (e) => {
+            //ajax 로 데이터 가져오기 
+
+            let req = new XMLHttpRequest();
+
+            req.open('GET', '/data/all');
+            //요청
+            req.send('');
+            //데이터 전송 
+            req.addEventListener("load", () => {
+                //json 문자열 확인
+                alert(req.responseText);
+            });
+        });
+```
+
+### 데이터 일부분 가져오기 
+> 데이터의 일부분 가져오기를 처리할 때는 일반적으로 2개의 데이터가 필요
+- 하나는 페이지 번호는 시작하는 데이터의 번호 
+    - 시작하는 데이터 번호를 주는 경우는 데이터의 개수가 고정인 경우가 많다. 
+
+- 다른 하나는 한 페이지에 출력할 데이터 개수
+
+### get방식에서의 서버로의 데이터 전달
+- 전달하고자 하는 데이터가 1개인 경우
+    - 파라미터로 전달 :URL?이름=값
+    - URL에 전달 : URL/값 - 최근에는 이 방식을 선호함
+
+- 전달하고자 하는 데이터가 2개 이상인 경우는 파라미터 형태로 전달
+    - URL?이름=값&이름=값... ... 
+
+```javascript
+// 파라미터로 읽기 
+   const page = req.params.page;
+    console.log(page);
+    // /data/all/ params...
+```
+
+
+### 정훈참조 라이브러리 bodyParser
+- https://medium.com/@chullino/1%EB%B6%84-%ED%8C%A8%ED%82%A4%EC%A7%80-%EC%86%8C%EA%B0%9C-body-parser%EB%A5%BC-%EC%86%8C%EA%B0%9C%ED%95%A9%EB%8B%88%EB%8B%A4-%ED%95%98%EC%A7%80%EB%A7%8C-body-parser%EB%A5%BC-%EC%93%B0%EC%A7%80-%EB%A7%88%EC%84%B8%EC%9A%94-bc3cbe0b2fd
+
+
+
+### Mysql 이나 MariaDb에서 데이터의 일부분을 가져오기 (TOP-N)
+- SELECT 구문의 마지막에 limit(시작하는번호 또는 건너뛸 데이터개수,데이터 개수)를 추가해주면 된다.
+
+```
+ ex 데이터개수 : 10 이면
+ 1페이지의 시작번호는 0
+ 2페이지의 시작번호는 10
+ 3페이지의 시작번호는 20
+ 4페이지의 시작번호는 30
+```
+
+- 건너뛸 데이터 개수는 = (페이지 번호-1)*데이터개수
+
+
+```javascript
+//item 테이블에서 itemid 를 가지고 내림차순 정렬해서
+    // 페이지 단위로 데이터 가져오기
+    // select * from item order by itemid desc limit 시작번호, 5
+    // 시작번호는 = (pageno-1)*5
+    // ? 자리는 저렇게 써준다 [(page-1)* 5]
+connection.query("select * from goods order by itemid desc limit ?, 5", [(pageno - 1) * 5], (err, results, fields) => { })
+ //파라미터는 무조건 문자열이다
+ // 파라미터를 가지고 산술연산을 할 때는 정수로 변환을 해주어야합니다.
+```
+
+```javascript
+//전체 데이터 가져올때 
+
+ connection.query("select * from goods order by itemid desc", [], (err, results, fields) => {
+        if (err) {
+            //err 가 발생한경우
+            // 에러가 발생했다고 데이터를 전송하지 않으면 안됨.
+            res.json({ 'result': false })
+            console.log(err);
+        } else {
+            res.json({ 'result': true, 'list': results });
+            //정상 응답을 한 경우
+        }
+    })
+```
+
+> 페이지 번호가 전체 데이터 개수보다 커지는 상황이 발생하면 데이터를 리턴하지 않는다
+- 클라이언트 측에서 서버에게 전체 데이터 이후의 데이터를 요청하지 못하도록 하고자 하면 이 경우에 는 전체 데이터의 개수를 전달해주면된다.
--- DB/11-24/server.js
+++ DB/11-24/server.js
@@ -1,46 +1,218 @@
-const mariadb = require('mysql');
+const express = require('express');
+const path = require('path');
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+//세션을 저장하기 위한 MYSQL 데이터베이스 저장소생성
 
-let connection = mariadb.createConnection({
-    host: '192.168.0.156',
-    port: 3306,
-    user: 'root',
-    password: '1234',
-    database: 'kjh'
+const morgan = require('morgan');
+const compression = require('compression');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const multer = require('multer');
+
+
+let fs = require('fs');
+let util = require('util');
+let mime = require('mime');
+let MariaDBStore = require('express-mysql-session')(session);
+let bodyParser = require('body-parser');
+let logDirectory = path.join(__dirname, 'log');
+let FileStreamRotator = require('file-stream-rotator');
+//로그를 기록할 디렉토리 경로설정
+
+
+
+dotenv.config();
+
+const app = express();
+
+app.set('port', process.env.PORT || 7500);
+
+const upload = multer({
+    Storage: multer.diskStorage({
+        destination(req, file, done) {
+            done(null, 'public/img');
+        },
+        filename(req, file, done) {
+            const ext = path.extname(file.originalname);
+            done(null, path.basename(file.originalname, ext) +
+                Date.now() + ext);
+        }
+    }),
+    limits: { fileSize: 10 * 1024 * 1024 }
 })
 
+//로그를 매일 기록하기 위한 설정
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
 
-//연결
+//로그 파일 옵션을 설정
+let accessLogStream = FileStreamRotator.getStream({
+    date_format: 'YYYYMMDD',
+    filename: path.join(logDirectory, 'access-%DATE%.log'),
+    frequency: 'daily',
+    verbose: false
+})
 
+let options = {
+    host: process.env.HOST,
+    port: process.env.MYSQLPORT,
+    user: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
+}
+
+
+
+let connection = mysql.createConnection(options);
 connection.connect((err) => {
     if (err) {
         console.log(err);
-        //에러발생했을떄
+        throw err;
     } else {
-        // console.log(connection);
-
-        // //테이블 생성구문
-        // connection.query('create table family(id int auto_increment primary key, name varchar(100))');
-        // //데이터 삽입 구문 
-        // connection.query('insert into family(name)values(?)','을지문덕');
-        // connection.query('insert into family(name)values(?)','김정훈');
-        // connection.query('insert into family(name)values(?)','오시훈');
-
-        //select 구문 
-        connection.query("select * from family ", (err, result, fields) => {
-            if (err) {
-                //이렇게하면 서버에서밖에안보인다.
-                //클라이언트 한테 보내줘야함
-                console.log("{result:false}");
-            } else {
-                //json문자열로 주겠다는얘기
-
-                let results = JSON.stringify(result);
-                console.log(results);
-                console.log(fields);
-            }
-        })
+        console.log("db도연결완료")
     }
 })
 
 
+app.use('/', express.static('public'));
+app.use(compression());
+app.use(morgan('combined', { stream: accessLogStream }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: new MariaDBStore(options)
+}))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'))
+});
+
+//여기까지함
+app.get('/data/all', (req, res) => {
+    let pageno = req.query.pageno;
+    if (pageno == undefined) {
+        pageno = 1;
+    }
+    console.log(pageno);
+
+    connection.query("select * from goods order by itemid desc limit ?, 5", [(parseInt(pageno - 1)) * 5], (err, results, fields) => {
+        if (err) {
+            res.json({ 'results': false })
+            console.log(err)
+        } else {
+            res.json({ 'results': true, 'list': results });
+        }
+    })
+
+
+    // connection.query("select * from goods order by itemid desc", [], (err, results, fields) => {
+    //     if (err) {
+    //         //err 가 발생한경우
+    //         // 에러가 발생했다고 데이터를 전송하지 않으면 안됨.
+    //         res.json({ 'result': false })
+    //         console.log(err);
+    //     } else {
+    //         res.json({ 'result': true, 'list': results });
+    //         //정상 응답을 한 경우
+    //     }
+    // })
+});
+
+
+app.get('/data/all/:page', (req, res) => {
+    const page = req.params.page;
+    console.log(page);
+});
+
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    req.status(500).send(err.message);
+});
+
+
+
+app.listen(app.get('port'), () => {
+    console.log(app.get('port'), '번 포트킴');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//=============================================================================================
+
+
+
+// const mariadb = require('mysql');
+// const dotenv = require('dotenv');
+
+// dotenv.config();
+
+// let connection = mariadb.createConnection({
+//     host: '192.168.0.156',
+//     port: 3306,
+//     user: 'root',
+//     password: '1234',
+//     database: 'kjh'
+// })
+
+
+//연결
+
+// connection.connect((err) => {
+//     if (err) {
+//         console.log(err);
+//         //에러발생했을떄
+//     } else {
+//         // console.log(connection);
+
+//         // //테이블 생성구문
+//         // connection.query('create table family(id int auto_increment primary key, name varchar(100))');
+//         // //데이터 삽입 구문
+//         // connection.query('insert into family(name)values(?)','을지문덕');
+//         // connection.query('insert into family(name)values(?)','김정훈');
+//         // connection.query('insert into family(name)values(?)','오시훈');
+
+//         //select 구문
+//         connection.query("select * from family ", (err, result, fields) => {
+//             if (err) {
+//                 //이렇게하면 서버에서밖에안보인다.
+//                 //클라이언트 한테 보내줘야함
+//                 console.log("{result:false}");
+//             } else {
+//                 //json문자열로 주겠다는얘기
+
+//                 let results = JSON.stringify(result);
+//                 console.log(results);
+//                 console.log(fields);
+//             }
+//         })
+//     }
+// })
+
+
 //데이터베이스 연결이 완료되었을떄 
+
+
+
+
+
+
+
+
+
