35d88a9fd1ce2392aa22cc5b470ca70cd0a5a2e3
jhva <junghun8158@naver.com>
2022-11-28 17:48:41 +0900

    user ,c,comment sequelize practice table add

--- DB/11-28/app.js
+++ DB/11-28/app.js
@@ -1,5 +1,8 @@
-const express = require("express");
 const path = require('path');
+const express = require("express");
+const User = require('./models/users');
+const Comment = require("./models/comments");
+
 
 const { sequelize } = require("./models");
 
@@ -12,6 +15,12 @@ sequelize.sync({ force: false })
     .then(() => { console.log("sequelize 연결완룡") })
     .catch((err) => { console.log(err, "연결실패") })
 
-app.listen(app.get('port'),()=>{
-    console.log(app.get("port"),'포트연결완료')
+app.get('/', (req, res) => {
+    const user = User.create({ name: "개바보", age: 24 })
+    console.log(user)
+})
+
+
+app.listen(app.get('port'), () => {
+    console.log(app.get("port"), '포트연결완료')
 })
